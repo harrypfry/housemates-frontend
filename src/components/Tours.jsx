0ba16bec514fa20dab8f2f403e4495d479a0a229
Harry Fry <harrypfry@gmail.com>
2020-02-03 12:20:29 +0000

    title of tour page complete

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Tours = () => {
-  return <div>Tours Page</div>;
-};
+import { Menu } from "semantic-ui-react";
+
+class Tours extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <div>
+        <div>
+          <h2 className="tour-page-header">Tours in Melbourne</h2>
+
+          <Menu className="tour-page-menu" text>
+            <Menu.Menu className="tour-page-menu-container" position="right">
+              <Menu.Item header className="tour-page-menu-item">
+                Sort By
+              </Menu.Item>
+              <Menu.Item
+                className="tour-page-menu-item"
+                name="az"
+                active={activeItem === "az"}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                className="tour-page-menu-item"
+                name="price"
+                active={activeItem === "price"}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                className="tour-page-menu-item"
+                name="rating"
+                active={activeItem === "rating"}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Tours;
